f0161bed5615dd497ccb002f3c67400c081394b7
sandroluisj <sandroluisjr06@gmail.com>
2024-10-01 16:57:12 -0300

    add

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
-const app = express()
+const dontenv = require('dotenv')
+const connectDB = require ("./config/db")
+const userRoutes = require('./routes/userRoutes')
 const timesRoutes = require("./routes/times")
+dontenv.config()
+connectDB()
+
+const app = express()
 app.use(express.json())
+app.use('/api', userRoutes)
 app.use("/times", timesRoutes)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 module.exports = app
